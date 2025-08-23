37ad84824cf4867df0801b32ab968686eb141d16
Elena Mannoni <elemannoni04@gmail.com>
2025-08-23 22:05:34 +0200

    Update models.py

--- src/models.py
+++ src/models.py
@@ -1,20 +1,16 @@
 import torch.nn as nn
+class MLP_4(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.fc = nn.Sequential(
+            nn.Linear(30, 1024),
+            nn.ReLU(),
+            nn.Linear(1024, 1024),
+            nn.ReLU(),
+            nn.Linear(1024, 512),
+            nn.ReLU(),
+            nn.Linear(512, 2)
+        )
 
-class MLP_celeba(nn.Module):
-  def __init__(self):
-    super().__init__()
-    self.fc = nn.Sequential(
-      nn.Flatten(),
-      nn.Linear(12288, 2048),
-      nn.ReLU(),
-      nn.Linear(2048, 1024),
-      nn.ReLU(),
-      nn.Linear(1024, 1024),
-      nn.ReLU(),
-      nn.Linear(1024, 512),
-      nn.ReLU(),
-      nn.Linear(512, 2)
-    )
-
-  def forward(self, x):
-    return self.fc(x)
+    def forward(self, x):
+        return self.fc(x)
